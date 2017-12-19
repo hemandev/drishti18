7ca117a5f781d84f589ba1dc1abeac3c15ecd3be
hemanditwiz <hemanditwiz@gmail.com>
2017-12-20 02:26:56 +0530

    added home page

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $('.heading').lettering()
+    $('.heading2').lettering()
+
+
+    $('#click').on('click', function () {
+       // $('.rounded-circle').toggleClass('anim')
+        //$('.rounded-circle').toggle()
+
+          $('.circle1').toggleClass('anim')
+          $('.circle2').toggleClass('anim1')
+        $('.circle3').toggleClass('anim2')
+        $('.circle4').toggleClass('anim3')
+        $('.circle5').toggleClass('anim4')
+        $('.circle6').toggleClass('anim5')
+        $('.circle7').toggleClass('anim6')
+
+
+    })
+})
