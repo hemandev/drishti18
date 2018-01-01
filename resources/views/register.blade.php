753f6344fec6fd0c6343539be87c203d543d0a88
Nilesh PS <nps17thatsme@gmail.com>
2018-01-01 19:49:39 +0530

    added register popup

---
+++ resources/views/register.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title', 'Drishti18-Registrations')
+
+
+@section('content')
+
+    <button onclick="popup('drishti-2018-324240');" class="tsbutton">Register Now</button>
+    <noscript id="tsNoJsMsg">Javascript on your browser is not enabled.</noscript>
+    <script src="//www.townscript.com/popup-widget/townscript-widget.nocache.js" type="text/javascript"></script>
+
+@endsection
